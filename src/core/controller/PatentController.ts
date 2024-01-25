29ab620c25a70e3b06201fe830c2f64c5ac8ed35
AhmadND00 <ahmadnd00@protonmail.com>
2024-01-25 13:28:22 +0700

    Updated CRUD for Patent
    - READ - http://localhost:19003/api/v1/patents
    - CREATE, UPDATE, DELETE
    http://localhost:19003/api/v1/{id}

--- src/core/controller/PatentController.ts
+++ src/core/controller/PatentController.ts
@@ -1,29 +1,83 @@
-import { IController } from 'core/shared/IController';
-import PatentWorker from '../worker/PatentWorker';
-import express from 'express';
+  import { IController } from 'core/shared/IController';
+  import PatentWorker from '../worker/PatentWorker';
+  import Patents from '../model/Patents';
+  import express from 'express';
 
-export class PatentController implements IController {
-  path = '/patents';
-  router = express.Router();
-  _worker: PatentWorker;
 
-  constructor() {
-    this._worker = new PatentWorker();
-    this.initRouter();
+
+
+  export class PatentController implements IController {
+    path = '/patents';
+    router = express.Router();
+    _worker: PatentWorker;
+
+    constructor() {
+      this._worker = new PatentWorker();
+      this.initRouter();
+    }
+
+    initRouter() {
+      this.router.get(`${this.path}`, this.getAllPatents);
+      this.router.post(`${this.path}/:patent_id`, this.addPatent);
+      this.router.delete(`${this.path}/:patent_id`, this.deletePatent);
+      this.router.put(`${this.path}/:patent_id`, this.updatePatent);
+    }
+
+
+    private getAllPatents = (req: express.Request, res: express.Response) => {
+      this._worker
+      .getAllPatent()
+      .then((data) => {
+        res.send(data);
+      }).catch((err) => {
+        res.send(err);
+      });
+    }
+
+    private addPatent = async (req: express.Request, res: express.Response) => {
+      try {
+        const patent: Patents = new Patents(
+          0,
+          req.body.title,
+          req.body.type,
+          req.body.noPatent,
+          req.body.date,
+          req.body.status
+        );
+
+        const patentData = await this._worker.addPatent(patent);
+        res.json(patentData);
+      } catch (err) {
+        res.json(err);
+      }
+    }
+
+    private updatePatent = async (req: express.Request, res: express.Response) => {
+    try {
+      const patentId = Number(req.params.patent_id);
+      const updatedPatent: Patents = {
+        id: patentId,
+        title: req.body.title,
+        type: req.body.type,
+        no_patent: req.body.noPatent,
+        date: req.body.date,
+        status: req.body.status,
+      };
+
+      const patentData = await this._worker.updatePatent(patentId, updatedPatent);
+      res.json(patentData);
+    } catch (err) {
+      res.json(err);
+    }
   }
 
-  initRouter() {
-    this.router.get(`${this.path}/get-all`, this.getAllPatents);
-  }
 
 
-  private getAllPatents = (req: express.Request, res: express.Response) => {
-    this._worker
-    .getAllPatent()
-    .then((data) => {
-      res.send(data);
-    }).catch((err) => {
-      res.send(err);
-    });
-  }
-}
+    private deletePatent = async (req: express.Request, res: express.Response) => {
+      try {
+        const patentData = await this._worker.deletePatent(Number(req.params.patent_id));
+        res.json(patentData);
+      } catch (err) {
+        res.json(err)
+      }
+    }}
--- src/core/model/Patents.ts
+++ src/core/model/Patents.ts
@@ -2,17 +2,17 @@ class Patents {
     id: number;
     title: string;
     type: string;
-    noPatent: string;
+    no_patent: string;
     date: Date;
     status: string;
     constructor(id: number, title: string, type: string, noPatent: string, date: Date, status: string) {
         this.id = id;
         this.title = title;
         this.type = type;
-        this.noPatent = noPatent;
+        this.no_patent = noPatent;
         this.date = date;
         this.status = status;
     }
-
-
 }
+
+export default Patents;
